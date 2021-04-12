b675d062c01332867ecec3bbbbf2c8b1903f37bd
vikash <vikashkumar754189@gmail.com>
2021-04-12 13:55:35 +0530

    creating a class method

--- oops/class1.cpp
+++ oops/class1.cpp
@@ -1,30 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//how to access private variable
 class Student{
  public:
   int rollNUmber;
+ private:
   int age;
+  public:
+  void display()
+  {
+    cout<<age<<" "<<rollNUmber<<endl;
+    //age and roll number can be acceseed easily as function is inside the class
+    //age value is garbage value as age is private variable and so cannot be set inside the main
+    
+  }
 };
 
-//how to create the Object and define its property value
+
 int main()
 {
-    Student s1; //object of student create statically
-    // assigning value to statically created obj
-     s1.age=24;
+    Student s1; 
+    // s1.age=24; can not be accessed as  age is private varible and it is outside the class 
      s1.rollNUmber=101;
 
-     cout<<s1.age<<" "<<s1.rollNUmber<<endl;
+    //how to call class method for statical variable
+    s1.display();
 
-    Student *s2=new Student; //object of student create dynamically
-    //assigning value to dynamically created object
-    (*s2).age=25;
+    Student *s2=new Student; 
+    //(*s2).age=25; can not be accessed as  age is private varible and it is outside the class 
     (*s2).rollNUmber=102;
-    // OR
-     s2->age=26;
-     s2->rollNUmber=103;
+    // calling display method for dynamical variable
+    (*s2).display();
 
-    cout<<(*s2).age<<" "<<s2->rollNUmber<<endl;
+    // OR
+    // s2->age=26;can not be accessed as  age is private varible and it is outside the class 
+     s2->rollNUmber=103;
+    // calling display method for dynamical variable
+     s2->display();
 
 }
