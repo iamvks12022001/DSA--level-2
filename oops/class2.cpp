f2c6345dc82916268544a7cae41bded32f22bf78
vikash <vikashkumar754189@gmail.com>
2021-04-13 17:49:30 +0530

    referrence variable

--- oops/class2.cpp
+++ oops/class2.cpp
@@ -19,19 +19,19 @@ class fraction{
     {
     	cout<<this->numerator<<" "<<denominator<<endl;
     }
-    void add(fraction f2)//fraction f2=main.f2 =>fraction f2(main.f2)=> copy constructor is called here.
-    {  
+    void add(fraction const &f2)   //fraction const & f2=main.f2
+    {  //if we want to stop copying then we can use refference variable here i.e add(function &f2)
+        //but by this here f2 can change the real (original) value of f2 so it's risky so we use const keyword
+        //so that in this block f2 can reffere the main f2 but can not change it's value from this block
+        
     	numerator=numerator*(f2.denominator) +  (f2.numerator*denominator);
     	denominator=denominator*(f2.denominator);
-    	//or
-    	// this->denominator=this->denominator*(f2.denominator);
-    	//note when we call a function by its oject for ex f1.add() then 
-    	//f1 address go inside the this keyword
+    
 
-      simpify(); // or  this->simplify();
+      simplify(); // or  this->simplify();
 
     }
-    void simpify(){
+    void simplify(){
     	int gcd=1;
     	for(int i=1;i<=min(numerator,denominator);i++)
     	{
@@ -44,9 +44,26 @@ class fraction{
     	this->denominator=this->denominator/gcd;
 
     }
-}
+    void multiply(fraction const & f2)
+    {
+        this->numerator=numerator*f2.numerator;
+        denominator=denominator*f2.denominator;
+
+        simplify();
+
+    }
+};
 
 int main()
 {
-   
+   fraction f1(10,2);
+   fraction f2(15,4);
+
+   f1.add(f2);
+   f1.print();
+   f2.print();
+
+   f1.multiply(f2);
+   f1.print();
+   f2.print();
 }
