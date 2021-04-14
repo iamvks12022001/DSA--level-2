19662a53f57128d219b65403d80c06bc31d686f8
vikash <vikashkumar754189@gmail.com>
2021-04-15 00:59:25 +0530

    automatic inc of static variable as obj created

--- oops2/class7.cpp
+++ oops2/class7.cpp
@@ -1,26 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//automatic increse of static variable as we create obj of student
 class Student{
     public:
     int roolNo;
     int age;
 
     static int totalstudent; //total number of student present
-
+     Student()
+     {
+         totalstudent++;
+     }
 };
-int Student ::totalstudent=0; //initialize static data members
+int Student ::totalstudent=0; 
 
 int main(){
-    Student s1;
+    Student s1,s2,s3,s4,s5;
 
-    cout<<s1.roolNo<< " "<<s1.age<<endl;
-    //cout<<s1.totalstudent ;// not give error but it's logically wrong
-
-    cout<<Student :: totalstudent<<endl;
-    s1.totalstudent=20;//this is idle wrong we know that but it work
-    Student s2;
-    cout<<s2.totalstudent<<endl; //give ans 20
-    cout<<Student:: totalstudent<<endl;//give 20 as a ans
+ cout<<Student :: totalstudent<<endl;  
 
 }
