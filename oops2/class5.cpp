0efbe26e905d348b00454ab4ad9da7eadcd66e59
vikash <vikashkumar754189@gmail.com>
2021-04-14 16:49:26 +0530

    intializing list for constant data member

--- oops2/class5.cpp
+++ oops2/class5.cpp
@@ -1,17 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//intializing the roolno so that no one can modify it in future.
 class Student {
 public:
 int age;
 const int rollno;
 
+
+Student(int r ):rollno(r)//=> const int rollno =r; so now rollno not get garbag value
+{
+}
+
 };
 
 int main()
 {
-    Student s1;// this will call deafult constructor and set a constant
-    // variale roll no as a garbage value so it give error
+    Student s1(233);
     s1.age=10;
-    s1.rollno=233;
+    //s1.rollno=233;
 }
