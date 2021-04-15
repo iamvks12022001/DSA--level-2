2ba138a3f1023949b716c9c8088650f25f589bbe
vikash <vikashkumar754189@gmail.com>
2021-04-15 18:05:14 +0530

    operator overloading -3

--- oops2/class8.cpp
+++ oops2/class8.cpp
@@ -74,37 +74,78 @@ class Fraction {
 		}
 
 		// Pre-increment
-		Fraction& operator++() { //use & so that it incrase in f1 always
+		Fraction& operator++() {
 			numerator = numerator + denominator;
 			simplify();
 
 			return *this;
 		}
 
+		// Post-increment
+		Fraction operator++(int) {
+			Fraction fNew(numerator, denominator);
+			numerator = numerator + denominator;
+			simplify();
+			fNew.simplify();
+			return fNew;
+		}
+
+
+		Fraction& operator+=(Fraction const &f2) {
+			int lcm = denominator * f2.denominator;
+			int x = lcm / denominator;
+			int y = lcm / f2.denominator;
+
+			int num = x * numerator + (y * f2.numerator);
+
+			numerator = num;
+		  denominator = lcm;
+			simplify();
+
+			return *this;
+		}
+
+
+
+
+
 };
 
 
 int main() {
-	Fraction f1(10, 2);
-	Fraction f2(15, 4);
 
+	int i = 5, j = 3;
+	(i += j) += j;
+
+	cout << i << " " << j << endl;
+
+	Fraction f1(10, 3);
+	Fraction f2(5, 2);
+
+	
+	(f1 += f2) += f2;
+
+	f1.print();
+	f2.print();
+
+
+
+	/*
+	Fraction f3 = f1++;
+	f1.print();
+	f3.print();
+	*/
+
+	/*
 	f1.print();
 	Fraction f3 = ++(++f1);
 	f1.print();
 	f3.print();
-
-
-
-
-
-	//++f1;
-	//f1.print();
-
-	/*
-	Fraction f3 = ++f1;
-	f1.print();
-	f3.print();
 	*/
+
+
+
+
 	
 }
 
