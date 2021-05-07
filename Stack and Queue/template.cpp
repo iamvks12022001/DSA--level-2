7da88cd1e5f57b1e45490a9aea7cb90c9271261d
vikash <vikashkumar754189@gmail.com>
2021-05-07 15:58:35 +0530

    template for 3 variable in same class

--- Stack and Queue/template.cpp
+++ Stack and Queue/template.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-template<typename T,typename V> //declare the temporary variable T
+template<typename T,typename V> 
 
 class Pair{
-    T x;   //use temporary data type T
-    V y;
+    T x;  
+    V y; 
     public:
 
     void setX(T x){
@@ -28,9 +28,8 @@ class Pair{
 
 int main()
 {
-  Pair<int,double>p1; //we need to define the data type during the creation of object
-               //class pair is of int and double data type data member
-  Pair<double,double>p2;//class pair is of double,double data type data member
+  Pair<int,double>p1; 
+  Pair<double,double>p2;
   
    p1.setX(10);
    p1.setY(30.8456);
@@ -43,5 +42,18 @@ int main()
    Pair<char,string>p3;
    p3.setX('a');
    p3.setY("Vikash Kumar Singh");
-   cout<<p3.getX()<<" "<<p3.getY();
+   cout<<p3.getX()<<" "<<p3.getY()<<endl;
+
+   Pair<Pair<int ,int>,int> p4;  //if we want to create three  data memeber in same class
+   p4.setY(10);                // p4 is object of pair type and int type
+   Pair<int,int>p5;
+   p5.setX(5);
+   p5.setY(16);
+   p4.setX(p5);
+
+   cout<<p4.getX().getX()<<" "<<p4.getX().getY()<<" "<<p4.getY()<<endl;
+
+   //if we want to create the pair of 3 data memeber of int double and char type
+   //then create p6 in this maanner Pair<Pair<int ,double>,char> and crate p7 of Pair<int,double>
+   // and use similary for 4 ,5 ..etc
 }
