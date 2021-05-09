d97fa5d9ca54b3b8747c49cb063ceb86ef505d60
vikash <vikashkumar754189@gmail.com>
2021-05-09 16:39:13 +0530

    parenthesis balanced

---
+++ Stack and Queue/balancedPar.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+       
+         stack<char> st;
+         for(int i=0;i<s.length();i++)
+         {
+             if(s[i]=='(' ||s[i]=='{' ||s[i]=='[')
+             {
+                st.push(s[i]);
+             }else{
+                 if((st.empty()==0)&&((s[i]==')' && st.top()=='(' )||(s[i]==']' && st.top()=='[' )||(s[i]=='}' && st.top()=='{' )))
+                 {
+                     st.pop();
+                 }else{
+                     return false;
+                 }
+                
+             }
+           
+         }
+        
+        return st.empty();
+    }
+};
+int main(){
+
+
+}
