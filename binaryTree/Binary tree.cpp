65a6a7070217315a34a15e20976242d52a778352
vikash <vikashkumar754189@gmail.com>
2021-06-03 16:00:53 +0530

    generate Binary tree from preorder and inorder traversl

--- binaryTree/Binary tree.cpp
+++ binaryTree/Binary tree.cpp
@@ -271,6 +271,37 @@ BinaryTreeNode<int>* buildTreefrom_In_PreOrder(vector<int> in,vector<int> pre, i
         return root;
     }
 
+BinaryTreeNode<int>* buildTreeHelper(int* in, int* pre, int inS, int inE, int preS, int preE) {
+	if (inS > inE) {
+		return NULL;
+	}
+
+	int rootData = pre[preS];
+	int rootIndex = -1;
+	for (int i = inS; i <= inE; i++) {
+		if (in[i] == rootData) {
+			rootIndex = i;
+			break;
+		}
+	}
+
+	int lInS = inS;
+	int lInE = rootIndex - 1;
+	int lPreS = preS + 1;
+	int lPreE = lInE - lInS + lPreS;
+	int rPreS = lPreE + 1;
+	int rPreE = preE;
+	int rInS = rootIndex + 1;
+	int rInE = inE;
+	BinaryTreeNode<int>*  root = new BinaryTreeNode<int>(rootData);
+	root->left = buildTreeHelper(in, pre, lInS, lInE, lPreS, lPreE);
+	root->right = buildTreeHelper(in, pre, rInS, rInE, rPreS, rPreE);
+	return root;
+}
+
+BinaryTreeNode<int>* buildTree(int* in, int* pre, int size) {
+	return buildTreeHelper(in, pre, 0, size - 1, 0, size - 1);
+}
 int main()
 {
     // BinaryTreeNode<int>* root=new BinaryTreeNode<int>(1);
@@ -319,6 +350,11 @@ int main()
    }
    BinaryTreeNode<int>* root1=buildTreefrom_In_PreOrder(in,pre,n);
    printLevelWise(root1);
+
+   int in[] = {4,2,5,1,8,6,9,3,7};
+	int pre[] = {1,2,4,5,3,6,8,9,7};
+	BinaryTreeNode<int>* root = buildTree(in, pre, 9);
+	printTree(root);
    delete root;// to delete the node recursively
 
     main();
