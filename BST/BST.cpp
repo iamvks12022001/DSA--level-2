93f1fd07ce88a8ff9fb1b6d29c3593a99b8d4229
vikash <vikashkumar754189@gmail.com>
2021-06-20 14:10:16 +0530

    BST Class

--- BST/BST.cpp
+++ BST/BST.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 template<typename T>
 class BinarySearchTree {
     public:
@@ -21,6 +22,126 @@ class BinarySearchTree {
           // so it is fine
      }
 
+};
+
+class BST {
+  BinarySearchTree<int>* root;
+
+  public:
+
+  BST() {
+    root = NULL;
+  }
+
+  ~BST() {
+    delete root;
+  }
+
+  private:
+  BinarySearchTree<int>* deleteData(int data, BinarySearchTree<int>* node) {
+    if (node == NULL) {
+      return NULL;
+    }
+
+    if (data > node->data) {
+      node->right = deleteData(data, node->right);
+      return node;
+    } else if (data < node->data) {
+      node->left = deleteData(data, node->left);
+      return node;
+    } else {
+      if (node->left == NULL && node->right == NULL) {
+        delete node;
+        return NULL;
+      } else if (node->left == NULL) {
+        BinarySearchTree<int>* temp = node->right;
+        node->right = NULL;
+        delete node;
+        return temp;
+      } else if (node->right == NULL) {
+        BinarySearchTree<int>* temp = node->left;
+        node->left = NULL;
+        delete node;
+        return temp; 
+      } else {
+        BinarySearchTree<int>* minNode = node->right;
+        while (minNode->left != NULL) {
+          minNode = minNode->left;
+        }
+        int rightMin = minNode->data;
+        node->data = rightMin;
+        node->right = deleteData(rightMin, node->right);
+        return node;
+      }
+    }
+  }
+
+  void printTree(BinarySearchTree<int>* root) {
+    if (root == NULL) {
+      return;
+    }
+    cout << root->data << ":";
+    if (root->left != NULL) {
+      cout << "L" << root->left->data;
+    }
+
+    if (root->right != NULL) {
+      cout << "R" << root->right->data;
+    }
+    cout << endl;
+    printTree(root->left);
+    printTree(root->right);
+  }
+
+
+  public:
+  void deleteData(int data) {
+    root = deleteData(data, root);  
+  }
+
+  void printTree() {
+    printTree(root);
+  }
+
+  private:
+  BinarySearchTree<int>* insert(int data, BinarySearchTree<int>* node) {
+    if (node == NULL) {
+      BinarySearchTree<int>* newNode = new BinarySearchTree<int>(data);
+      return newNode;
+    }
+
+    if (data < node->data) {
+      node->left = insert(data, node->left);
+    } else {
+      node->right = insert(data, node->right);
+    }
+    return node;
+  }
+
+  public:
+  void insert(int data) {
+    this->root = insert(data, this->root);
+  }
+
+  private:
+  bool hasData(int data, BinarySearchTree<int>* node) {
+    if (node == NULL) {
+      return false;
+    }
+
+    if (node->data == data) {
+      return true;
+    } else if (data < node->data) {
+      return hasData(data, node->left);
+    } else {
+      return hasData(data, node->right);
+    }
+  }
+
+  public:
+  bool hasData(int data) {
+    return hasData(data, root);
+  }
 };
 
 BinarySearchTree<int>* takeInputLevelWise()
@@ -290,7 +411,7 @@ vector<int>* getRootToNodePath(BinarySearchTree<int>*root,int data)
 int main()
 {
     
-    BinarySearchTree<int>* root=takeInputLevelWise();
+  //  BinarySearchTree<int>* root=takeInputLevelWise();
     // cout<<"give data to search"<<endl;
     // int val;
     // cin>>val;
@@ -319,13 +440,26 @@ int main()
     //     head1=head1->right;
     // }
 
-    vector<int>* output=getRootToNodePath(root,8);
-    for(int i=0;i<output->size();i++)
-    {
-        cout<<output->at(i)<<endl;
-    }
-    delete output;
-    delete root;// to delete the node recursively
-   main();
+    // vector<int>* output=getRootToNodePath(root,8);
+    // for(int i=0;i<output->size();i++)
+    // {
+    //     cout<<output->at(i)<<endl;
+    // }
+    // delete output;
+
+    BST b;
+	b.insert(10);
+	b.insert(5);
+	b.insert(20);
+	b.insert(7);
+	b.insert(3);
+	b.insert(15);
+	b.printTree();
+	b.deleteData(10);
+	b.deleteData(100);
+	b.printTree();
+    
+  //  delete root;// to delete the node recursively
+   
    
 }
